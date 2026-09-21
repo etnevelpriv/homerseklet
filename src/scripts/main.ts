61a223b3ef823607ad1fece59d9b16b646471322
etnevelpriv <leventerastovits@gmail.com>
2026-09-21 08:31:14 +0200

    fetch muvelet keszen van

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,9 +1,25 @@
-import { Homerseklet } from "./models/Homerseklet.Class";
-import { Homerseklet_Interface } from "./models/Homerseklet.Interface";
+import type { Homerseklet } from "./models/Homerseklet.Class";
+import type { Homerseklet_Interface } from "./models/Homerseklet.Interface";
 import "../styles/style.css";
 
-const init = function() {
-
+const init = async function() {
+    const idojarasData = await fetchIdojaras();
+    console.log(idojarasData)
 }
+
+const fetchIdojaras = async function() {
+    const URL = "https://petrik-idojaras-default-rtdb.europe-west1.firebasedatabase.app/.json";
+    try {
+        const response = await fetch(URL, {
+            method: "GET"
+        });
+        if (response.ok) {
+            const data = response.json();
+            return(data)
+        }
+    } catch (err:Error|any) {
+        throw new Error(err);
+    };
+};
 
 document.addEventListener("DOMContentLoaded", init);
